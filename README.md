41869cce470f614645df285614eec9e5b6b58c69
Priyanshu Rai <idealpriyanshu@gmail.com>
2022-12-16 15:49:44 +0530

    autoupdate

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # rare-diseases-data-scraping
 
 `data.csv` is the data extracted from the big list at https://rarediseases.info.nih.gov/diseases while `data1.csv` is the currently incomplete detailed description about the diseases.
+
+`data.csv` was retrieved using `raredsel.py`.
---
+++ raredsel.py
@@ -0,0 +1,58 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+driver_path = '/usr/bin/geckodriver'
+service = Service(driver_path)
+ff_options = Options()
+# ff_options.add_argument('--headless')
+ff_options.add_argument('--window-size=1920,1080')
+driver = Firefox(service=service, options=ff_options)
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+no_of_pages = 591
+
+base_url = "https://rarediseases.info.nih.gov/diseases"
+
+data = []
+df = pd.DataFrame(data)
+
+driver.get(base_url)
+
+timeout = 30
+try:
+    WebDriverWait(driver, timeout).until(EC.visibility_of_element_located((By.CLASS_NAME, "results")))
+except TimeoutException:
+    driver.quit()
+
+for i in range(1, 592):
+    url_link = driver.current_url
+    print(url_link)
+    s1 = BeautifulSoup(driver.page_source, 'html.parser')
+    results_div = s1.find('div', {'class': 'results pt-lg-2'})
+
+    for a_tag in results_div.find_all('a'):
+        d = {}
+        title = a_tag.find('h5').string
+        details = a_tag.find('p')
+        if details is None:
+            details = ""
+        else:
+            details = details.string
+        link = a_tag['href']
+        d['title'] = title
+        d['details'] = details
+        d['link'] = link
+        dict = pd.DataFrame(d, index=[0])
+        df = pd.concat([df, dict], ignore_index=True)
+        df.to_csv('/home/me/Documents/Internships/data-scraping/data.csv', index=False)
+
+    driver.find_element(By.XPATH, '/html/body/app-root/ng-component/div[2]/div/div/div/div/div[2]/div[3]/div/a[2]').click()
+driver.close()
